154f10d7646d6c8bb065b63ebe08832e1e4a9145
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2024-12-26 10:52:47 +0300

    test: добавляет тестовый комментарий

---
+++ source/js/modules/animations/init-animation-rules-page.js
@@ -0,0 +1,30 @@
+import {observeClassAddition} from "../utils/observeClassAddition";
+
+const initAnimationRulesPage = () => {
+  const lists = document.querySelectorAll(`[data-animation="list"]`);
+  const rulesPage = document.getElementById(`rules`);
+  const btn = rulesPage.querySelector(`.rules__link`);
+  // test
+
+  if (!lists.length || !rulesPage) {
+    return;
+  }
+
+  const animationCallback = () => {
+    const delay = 200;
+    lists.forEach((list) => {
+      const items = Array.from(list.children);
+      items.forEach((item, index) => {
+        setTimeout(() => {
+          item.classList.add(`is-active`);
+        }, delay * (index + 1));
+      });
+    });
+
+    btn.style.opacity = `1`;
+  };
+
+  observeClassAddition(rulesPage, `active`, animationCallback);
+};
+
+export {initAnimationRulesPage};
